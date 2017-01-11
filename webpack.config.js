94c374fe19f5c6674b4e621ef1de3ab3ca31a846
Jake Wilson <jakewilson780@gmail.com>
2017-01-11 04:00:02 -0500

    webkit/compoents with JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	entry: "../src/index.js",
+	entry: "./src/index.js",
 	output: {
-		path: __dirname + '/pubic',
+		path: __dirname + '/public',
 		filename: 'bundle.js'
 	},
 	module: {
