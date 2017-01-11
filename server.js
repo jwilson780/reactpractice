e47460fcddcc66d98ea7c37e8d565ffe8a28f89c
Jake Wilson <jakewilson780@gmail.com>
2017-01-11 02:33:57 -0500

    express routing

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-import http from 'http';
+import express from 'express';
+//import fs from 'fs';
+import config from './config';
+import apiRouter from './api';
 
-const server = http.createServer((req, res) =>{
+const server = express();//creates server in express
 
-	res.write("Hello HTTP\n");
-	setTimeout(() => {
-		res.write('I can stream \n');
-		res.end();
-	}, 3000);
+server.get('/', (req, res) =>{
+	res.send('Hello express\n');
 });
 
-server.listen(8080);
+//need to use nginX to handle static assets in production
+//express will take care of serving the content when requested
+
+server.use('/api', apiRouter);
+
+server.use(express.static('public'));
+
+//Manually routing using FS
+//	fs.readFile('./about.html', (err,data) =>{
+//		res.send(data.toString());//callback gives acces to files data as a buffer
+//	});
+//});
+
+server.listen(config.port, () =>{
+	console.info("express listening on port", config.port);
+});
