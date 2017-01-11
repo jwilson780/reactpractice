cf6a04ea8c87be12ea384a637ab8d2f5525f3fa2
Jake Wilson <jakewilson780@gmail.com>
2017-01-11 03:06:32 -0500

    cleaned up ejs

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-import express from 'express';
-//import fs from 'fs';
 import config from './config';
 import apiRouter from './api';
 
-const server = express();//creates server in express
-
-//need to use nginX to handle static assets in production
-//express will take care of serving the content when requested
+import express from 'express';
+const server = express();
 
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
-	res.render('index', {
-		content: 'Hello Express and <em>EJS</em>!'
-	});
+  res.render('index', {
+    content: '...'
+  });
 });
 
 server.use('/api', apiRouter);
-
 server.use(express.static('public'));
 
-//Manually routing using FS
-//	fs.readFile('./about.html', (err,data) =>{
-//		res.send(data.toString());//callback gives acces to files data as a buffer
-//	});
-//});
-
-server.listen(config.port, () =>{
-	console.info("express listening on port", config.port);
+server.listen(config.port, () => {
+  console.info('Express listening on port', config.port);
 });
